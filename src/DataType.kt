e50c5e40215aae2aa841230707e85eeb0f89a68b
Winnie <>
2021-01-10 19:54:06 +0900

    자료형선언, nullable

---
+++ src/DataType.kt
@@ -0,0 +1,17 @@
+//var 변수 : 타입 = 데이터
+val value : String = "수정 불가능한 값"
+val value2 = "수정 불가능한 값." //줌데이터를 보고 타입알아서 지정해줌var variable : String = "수정 가능한 값"
+
+var byte : Byte = 127
+var char : Char = 'a'
+var short: Short = 12345
+var int : Int = 4
+var long : Long = 3
+
+var float : Float = 3.14f
+var double : Double = 0.234
+
+var boolean : Boolean = true
+var string : String = "이"
+
+var nullable : Int? = null
